d9dae06732068a158ee243e7560a11b6cdfcb0c5
SAMA1981 <marceloarielsamaniego@gmail.com>
2023-10-29 19:02:32 -0300

    Pproyecto MarcketPlace

---
+++ views/modules/newletter.php
@@ -0,0 +1,71 @@
+<div class="ps-newsletter">
+
+    <div class="container">
+
+        <form class="ps-form--newsletter" action="do_action" method="post">
+
+            <div class="row">
+
+                <div class="col-xl-5 col-12 ">
+                    <div class="ps-form__left">
+                        <h3>Newsletter</h3>
+                        <p>Subcribe to get information about products and coupons</p>
+                    </div>
+                </div>
+
+                <div class="col-xl-7 col-12 ">
+
+                    <div class="ps-form__right">
+
+                        <div class="form-group--nest">
+
+                            <input class="form-control" type="email" placeholder="Email address">
+                            <button class="ps-btn">Subscribe</button>
+
+                        </div>
+
+                    </div>
+
+                </div>          
+
+            </div>
+
+        </form>
+
+        <br>
+                <center>
+                    <div>
+                        <iframe 
+                            style=
+                                "width:320px;
+                                height:260px;
+                                border-radius:10px;
+                                box-shadow:2px 4px 4px rgb(0 0 0 / 25%);
+                                display:flex;
+                                justify-content:center;
+                                border:1px solid #bcbcbc" 
+                            src="https://dolarhoy.com/i/cotizaciones/dolar-bancos-y-casas-de-cambio" 
+                            frameborder="0">
+                        </iframe>
+                    </div>
+
+                    <!--<div>
+                        <iframe 
+                            style=
+                                "width:320px;
+                                height:260px;
+                                border-radius:10px;
+                                box-shadow:2px 4px 4px rgb(0 0 0 / 25%);
+                                display:flex;
+                                justify-content:center;
+                                border:1px solid #bcbcbc" 
+                            src="https://dolarhoy.com/i/cotizaciones/dolar-blue" 
+                            frameborder="0">
+                        </iframe>
+                    </div>-->
+          
+                </center>
+
+    </div>
+
+</div>
